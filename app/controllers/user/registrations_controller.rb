10148369f6154d1faa2bbd5bc5cd86998986e36e
sfjdkfjjfjfdk <112384285+sfjdkfjjfjfdk@users.noreply.github.com>
2022-11-22 12:05:36 +0000

    新規会員登録の入力欄を追加

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -43,7 +43,7 @@ class User::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_up_params
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :event])
    end
 
    def after_sign_up_path_for(resource)
