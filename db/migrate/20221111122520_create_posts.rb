b644b9eef433b8b2093df5a5878d36f9e2e86fe6
sfjdkfjjfjfdk <112384285+sfjdkfjjfjfdk@users.noreply.github.com>
2022-11-22 10:59:50 +0000

    migate編集

--- db/migrate/20221111122520_create_posts.rb
+++ db/migrate/20221111122520_create_posts.rb
@@ -2,13 +2,13 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.bigint :user_id, null: false
-      t.string :name, null: false, default: ""
+      t.string :name, null: false
       t.date :date, null: false
-      t.text :time, null: false, default: ""
-      t.text :weather, null: false, default: ""
-      t.text :practice, null: false, default: ""
-      t.text :skill, null: false, default: ""
-      t.text :improvement, null: false, default: ""
+      t.text :time, null: false
+      t.text :weather, null: false
+      t.text :practice, null: false
+      t.text :skill, null: false
+      t.text :improvement, null: false
 
       t.timestamps
     end
