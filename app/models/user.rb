2ea0f54c8e741e5d601426ee0be02e4a740b1ce0
sfjdkfjjfjfdk <112384285+sfjdkfjjfjfdk@users.noreply.github.com>
2022-11-22 11:46:22 +0000

    user.model変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
  validates :name, presence: true
- validates :event, presence: true
- validates :target, presence: true
- validates :task, presence: true
 
-
-
- 
   has_many :posts, dependent: :destroy
 
  # いいねのアソシエーション
  has_many :likes, dependent: :destroy
  has_many :liked_posts, through: :likes, source: :post
-  
+
  def liked_by?(post_id)
      likes.where(post_id: post_id).exists?
  end
