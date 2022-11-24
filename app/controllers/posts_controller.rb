3b70154d8ea2524a5fe35e7437afff24c014a504
sfjdkfjjfjfdk <112384285+sfjdkfjjfjfdk@users.noreply.github.com>
2022-11-24 13:49:52 +0000

    新規登録、ログイン、会員情報ページのレイアウト

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
   end
 
   def index
-     @posts = Post.all.order("created_at DESC")
+     @posts = Post.page(params[:page]).order("created_at DESC")
      @post = current_user
   end
 
