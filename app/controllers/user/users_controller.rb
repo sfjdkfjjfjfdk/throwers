8d33ee449f7bd6ffea2eb9d13df2777c3c4ded31
sfjdkfjjfjfdk <112384285+sfjdkfjjfjfdk@users.noreply.github.com>
2022-11-25 11:07:27 +0000

    プロフィール画像を表示する機能を追加

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -57,7 +57,7 @@ class User::UsersController < ApplicationController
  private
 
  def user_params
-   params.require(:user).permit(:name, :event, :target, :task)
+   params.require(:user).permit(:name, :event, :target, :task, :profile_image)
  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,17 @@ class User < ApplicationRecord
  validates :name, presence: true
  validates :event, presence:true
 
-  has_many :posts, dependent: :destroy
+ has_one_attached :profile_image
+
+ def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      profile_image.variant(resize_to_limit: [width, height]).processed
+ end
+
+ has_many :posts, dependent: :destroy
 
  # いいねのアソシエーション
  has_many :likes, dependent: :destroy
