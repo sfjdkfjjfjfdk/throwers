7492536fa7bb4f4b31ac039c252649e128e506e9
sfjdkfjjfjfdk <112384285+sfjdkfjjfjfdk@users.noreply.github.com>
2023-03-27 14:01:59 +0000

    管理者側の会員情報の編集機能の作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,14 +1,35 @@
 class Admin::UsersController < ApplicationController
-  
+
  def index
    @users = User.all
  end
- 
+
  def show
    @user = User.find(params[:id])
  end
- 
+
  def edit
+   @user = User.find(params[:id])
+   if @user == current_user
+      redirect_to admin_user_path
+   else
+     render :edit
+   end
  end
- 
+
+ def update
+   @user = User.find(params[:id])
+   if @user.update(user_params)
+     redirect_to  admin_user_path
+   else
+     render :edit
+   end
+ end
+
+ private
+
+ def user_params
+   params.require(:user).permit(:name, :event, :profile_image)
+ end
+
 end
