e89c5bf6cfa07957d97f0cdb9bd19d417b0f8fca
sfjdkfjjfjfdk <112384285+sfjdkfjjfjfdk@users.noreply.github.com>
2023-04-12 11:32:55 +0000

    DM機能のコントローラの記述編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,14 @@
 class MessagesController < ApplicationController
  
-  def create
-      if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
-        @message = Message.new(message_params)
-        if @message.save
-            redirect_to "/rooms/#{@message.room_id}"
-        end
-      else
-        redirect_back(fallback_location: root_path)
-      end
-  end
-
-    private 
-    
-    def message_params
-        params.require(:message).permit(:user_id, :body, :room_id).merge(user_id: current_user.id)
+def create
+    # 条件がtrueだったら、メッセージを保存するためにMessage.createとし、Messagesテーブルにuser_id、:content、room_idのパラメーターとして送られてきた値を許可
+    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
+      # メッセージを送ったのは現在ログインしているユーザーなので、そのuser_idの情報をmerge
+      @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(user_id: current_user.id))
+    else
+      flash[:alert] = "メッセージ送信に失敗しました。"
     end
-    
+　　redirect_to "/rooms/#{@message.room_id}"
+  end
+  
 end
