75f80a6924c776e0494c689c0c2f5281f66dd4f1
sfjdkfjjfjfdk <112384285+sfjdkfjjfjfdk@users.noreply.github.com>
2023-04-19 09:33:14 +0000

    DMが送信できるようになった デザインがまだ来ていない

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,22 @@
 class MessagesController < ApplicationController
- 
+
 def create
     # 条件がtrueだったら、メッセージを保存するためにMessage.createとし、Messagesテーブルにuser_id、:content、room_idのパラメーターとして送られてきた値を許可
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
-      # メッセージを送ったのは現在ログインしているユーザーなので、そのuser_idの情報をmerge
-      @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(user_id: current_user.id))
+      @message = Message.new(message_params)
+        if @message.save
+            redirect_to "/rooms/#{@message.room_id}"
+        end
     else
       flash[:alert] = "メッセージ送信に失敗しました。"
     end
-　　# redirect_to "/rooms/#{@message.room_id}"
+    # redirect_to "/rooms/#{@message.room_id}"
 end
-  
+
+ private
+
+ def message_params
+    params.require(:message).permit(:user_id, :body, :room_id).merge(user_id: current_user.id)
+ end
+
 end
