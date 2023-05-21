636c0eee991f85e408d5ca7fdf8194a811808bdf
sfjdkfjjfjfdk <112384285+sfjdkfjjfjfdk@users.noreply.github.com>
2023-05-21 10:51:00 +0000

    いいねの非同期実装途中

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,9 +5,9 @@ class CommentsController < ApplicationController
      @comment = current_user.comments.new(comment_params)
      @comment.post_id = @post.id
      if @comment.save
-       redirect_to post_path(@post), notice: 'コメントを投稿しました'
+      redirect_to post_path(@post), notice: 'コメントを投稿しました'
      else
-       render 'posts/show'
+      render 'posts/show'
      end
   end
 
@@ -15,7 +15,6 @@ class CommentsController < ApplicationController
      Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
      redirect_to post_path(params[:post_id]), alert: 'コメントを削除しました'
   end
-
 
   private
 
