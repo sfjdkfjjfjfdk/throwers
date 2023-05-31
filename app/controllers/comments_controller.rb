df5d4637f5e220935da568d7c54302615605d0ca
sfjdkfjjfjfdk <112384285+sfjdkfjjfjfdk@users.noreply.github.com>
2023-05-31 09:01:17 +0000

    いいね非同期実装途中

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,16 +4,16 @@ class CommentsController < ApplicationController
      @post = Post.find(params[:post_id])
      @comment = current_user.comments.new(comment_params)
      @comment.post_id = @post.id
-     if @comment.save
-      redirect_to post_path(@post), notice: 'コメントを投稿しました'
-     else
-      render 'posts/show'
-     end
+     @comment.save
+      # redirect_to post_path(@post), notice: 'コメントを投稿しました'
+     # else
+     #  render 'posts/show'
+     # end
   end
 
   def destroy
      Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
-     redirect_to post_path(params[:post_id]), alert: 'コメントを削除しました'
+     # redirect_to post_path(params[:post_id]), alert: 'コメントを削除しました'
   end
 
   private
