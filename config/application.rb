4de196ee21994a3166723aa781c2633da4045a32
sfjdkfjjfjfdk <112384285+sfjdkfjjfjfdk@users.noreply.github.com>
2023-06-21 11:15:10 +0000

    非同期通信を実装　まだ未完成

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Throwers
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-     config.time_zone = "Tokyo"
+    config.time_zone = 'Tokyo'
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
